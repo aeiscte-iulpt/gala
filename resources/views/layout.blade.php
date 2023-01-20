43e7654f9a8f43979fdaccd19337dd819ce58a7a
davidlgabriel <david.g.leal00@gmail.com>
2023-01-20 02:01:44 +0000

    d

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <script data-queue-fair-client="client-name" src="https://files.queue-fair.net/queue-fair-adapter.js"></script>
+    <script data-queue-fair-client="gala" src="https://files.queue-fair.net/queue-fair-adapter.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
