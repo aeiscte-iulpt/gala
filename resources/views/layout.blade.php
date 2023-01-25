aac809e1d56553861b7b2cd8818940efba0efd17
davidlgabriel <david.g.leal00@gmail.com>
2023-01-25 03:49:15 +0000

    d

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,9 +21,7 @@
     <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js'></script>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css'>
 
-    <script defer src="https://friconix.com/cdn/friconix.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
 </head>
 
 <style>
