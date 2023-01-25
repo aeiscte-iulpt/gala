22f57f59063abb31cb7321972502eab7209a8f36
davidlgabriel <david.g.leal00@gmail.com>
2023-01-25 00:06:31 +0000

    d

--- app/Http/Controllers/GalaController.php
+++ app/Http/Controllers/GalaController.php
@@ -88,7 +88,7 @@ class GalaController extends Controller
         $ipfind= Inscritos::where('ip',$request->ip())->first();
         if (!isset($ipfind)) {
 
-        if ($count6 < 78) {
+        if ($count6 < 1) {
         if(!isset($inscrito1)){
             $users = new Inscritos;
             $users->nome_mesa   = $request->nome_mesa;
