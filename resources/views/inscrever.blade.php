c551930604d5ef6817b73efa8ee1c1956d98127a
davidlgabriel <david.g.leal00@gmail.com>
2023-02-05 02:56:02 +0000

    h

--- resources/views/inscrever.blade.php
+++ resources/views/inscrever.blade.php
@@ -517,14 +517,7 @@
   justify-content: center;
 }
 
-#spinner {
-  border: 5px solid rgba(255, 255, 255, 0.2);
-  border-left-color: #fff;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  animation: spin 1s linear infinite;
-}
+
 
 @keyframes spin {
   to { transform: rotate(360deg); }
